f07100931c2264c94c6b8d46d7d4ee3114d87422
Oscar Gonzalez <oigonzalezp2024@gmail.com>
2025-08-14 17:34:13 -0500

    refactor: Ajuste a la funcionalidad de editar mensaje

--- src/infrastructure/leerChat.php
+++ src/infrastructure/leerChat.php
@@ -62,6 +62,7 @@ function leerChat(string $ruta_json): string
             <iframe width="560" height="315" src="https://www.youtube.com/embed/' . $codigoVideo . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             ';
         }
+        $_SESSION['id_mensaje'] = (int) $mensaje['id_mensaje'];
     }
 
     return $content;
